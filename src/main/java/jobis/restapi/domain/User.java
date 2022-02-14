dbb012b17054dcd2fa8b7b65ea5c05f693585244
서국화 <kookhwa836@gmail.com>
2022-02-14 15:05:53 +0900

    사용자 정보 필수입력 validation 추가

--- src/main/java/jobis/restapi/domain/User.java
+++ src/main/java/jobis/restapi/domain/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
@@ -13,12 +14,16 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
+    @NotEmpty
     private String userId;
 
+    @NotEmpty
     private String name;
 
+    @NotEmpty
     private String password;
 
+    @NotEmpty
     private String regNo;
 
     public void setRegNo(String regNo) {
